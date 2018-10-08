efe33f3d4f7b7d113900b6cd2bf1ec89aea203ca
merv1618 <43761721+merv1618@users.noreply.github.com>
2018-10-08 14:03:15 -0500

    Update factor.py

--- factor.py
+++ factor.py
@@ -2,21 +2,20 @@ def factor(x):
     import math
     breakdown = "factorization: "
     y = 2
-    while y < x:
+    while y <= x:
         if x%y != 0:
             breakdown = .join(breakdown, str(x/y), "*")
             x = x/y
         else:
             y = y + 1
-    if y == x:
+    if y > x:
         break
     print(breakdown - "*")
 
 
-factor(27)
 # introduce temporary variable y
-#create a string
-# start a while looop
+# create a string
+# start a while loop
 # divide x by y until x % t != 0
 # concatenating each y to string
 # repeat process for increasing values of y until y > x
