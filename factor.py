4f388d09aaf6cbda34114e0e72b6965da241c5c8
merv1618 <43761721+merv1618@users.noreply.github.com>
2018-10-08 14:54:27 -0500

    Update factor.py
    Program for factoring integers

--- factor.py
+++ factor.py
@@ -1,10 +1,10 @@
 def factor(x):
-   breakdown = ""
+    breakdown = ""
    # final list of prime numbers
     y = 2
    # lowest prime
     while y <= x:
-        if x%y != 0:
+        if x%y == 0:
             breakdown = breakdown + "*" + str(y)
             # concatenates instance of prime number to final list
             x = x/y
@@ -13,6 +13,5 @@ def factor(x):
         else:
             y = y + 1
     if y > x:
-        break
-    print(breakdown[1:])
-   # removes the unnecessary asterisk
+        print(breakdown[1:])
+    # removes the unnecessary asterisk
